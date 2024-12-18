5f72e2bf7957c49466b563c8b9453b072b129f6d
luckycod3r <leni4kostenko@yandex.ru>
2024-12-18 08:01:31 +0300

    test

---
+++ dist/js/about.5a9d398e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgeolocation_island65"]=self["webpackChunkgeolocation_island65"]||[]).push([[594],{603:function(n,a,t){t.r(a),t.d(a,{default:function(){return r}});var u=t(6768);const e={class:"about"};function o(n,a){return(0,u.uX)(),(0,u.CE)("div",e,a[0]||(a[0]=[(0,u.Lk)("h1",null,"This is an about page",-1)]))}var s=t(1241);const c={},i=(0,s.A)(c,[["render",o]]);var r=i}}]);
+//# sourceMappingURL=about.5a9d398e.js.map
